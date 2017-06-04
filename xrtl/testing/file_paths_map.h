e53c242016b2173e7082463e41f202cddd32b246
Scott Todd <scotttodd@google.com>
2017-06-03 23:13:40 -0700

    Map test data relative paths to absolute paths using manifest.
    This gets image_comparison_test passing on Windows.
    
    Normally, test data files are symlinked into the runfiles directory. Bazel is not symlinking runfiles on Windows (yet) though.

---
+++ xrtl/testing/file_paths_map.h
@@ -0,0 +1,48 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef XRTL_TESTING_FILE_PATHS_MAP_H_
+#define XRTL_TESTING_FILE_PATHS_MAP_H_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "xrtl/base/string_view.h"
+
+namespace xrtl {
+namespace testing {
+
+// Mapping from test data relative paths to absolute paths.
+// If runfiles are fully supported, relative paths should work on their own.
+class FilePathsMap {
+ public:
+  // Parses the file paths map from the runfiles MANIFEST file.
+  // Call this during test setup.
+  static void ParseFromManifest();
+
+  // Gets the absolute path for the provided test data relative path or empty
+  // string if that exact relative path does not exist in the runfiles MANIFEST.
+  // Only files specified in the "data" field of tests will be available here.
+  static StringView get_absolute_path(StringView relative_path);
+
+ private:
+  static std::vector<std::pair<std::string, std::string>> file_paths_map;
+  static std::string workspace_name;
+};
+
+}  // namespace testing
+}  // namespace xrtl
+
+#endif  // XRTL_TESTING_FILE_PATHS_MAP_H_
