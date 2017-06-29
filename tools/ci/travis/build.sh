b215d15cc03a3011d908b61fdc02911298073e61
Ben Vanik <ben.vanik@gmail.com>
2017-06-29 10:06:50 -0700

    Adding Travis CI build.

---
+++ tools/ci/travis/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export CC=clang-4.0
+  export CXX=clang++-4.0
+else
+  export CC=clang
+  export CXX=clang
+fi
+
+# Use CI-specific bazel configurations.
+cp tools/ci/travis/.bazelrc .
+
+./xtool build --all
